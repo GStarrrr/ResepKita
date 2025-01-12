6c31cf7f2d4bd04a336570a4bdedf8f31d09edc6
ririks <ririkomalasari870@gmail.com>
2025-01-12 22:03:55 +0700

    memperbarui button

--- lib/signin.dart
+++ lib/signin.dart
@@ -1,6 +1,5 @@
 import 'package:coba/homepage.dart';
 import 'package:flutter/material.dart';
-// Pastikan file homepage.dart sudah ada
 import 'signup.dart';
 
 class Signin extends StatelessWidget {
@@ -9,118 +8,159 @@ class Signin extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Stack(
-      children: [
-        Container(
-          height: double.infinity,
-          width: double.infinity,
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(colors: [
-              Color(0xFFFFA726),
-              Color(0xFFFFCC80),
-            ]),
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.only(top: 300.0),
-          child: Container(
-            decoration: const BoxDecoration(
-              borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(30), topRight: Radius.circular(30)),
-              color: Colors.white,
-            ),
+      body: Stack(
+        children: [
+          // Latar belakang gradient
+          Container(
             height: double.infinity,
             width: double.infinity,
-            child: SingleChildScrollView(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(
-                    horizontal: 16.0, vertical: 24.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    const TextField(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Color(0xFFFFA726),
+                  Color(0xFFFFCC80),
+                ],
+              ),
+            ),
+          ),
+          // Memusatkan ikon dan teks
+          const Align(
+            alignment: Alignment.topCenter,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                SizedBox(height: 100), // Jarak dari atas
+                Icon(
+                  Icons.book,
+                  size: 100,
+                  color: Colors.white,
+                ),
+                SizedBox(height: 16),
+                Text(
+                  'Welcome to ResepKita',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          // Container putih di bawah
+          Padding(
+            padding: const EdgeInsets.only(top: 300.0),
+            child: Container(
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(30),
+                  topRight: Radius.circular(30),
+                ),
+                color: Colors.white,
+              ),
+              height: double.infinity,
+              width: double.infinity,
+              child: SingleChildScrollView(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 16.0,
+                    vertical: 24.0,
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      const TextField(
                         decoration: InputDecoration(
-                            prefixIcon: Icon(
-                              Icons.email,
+                          prefixIcon: Icon(
+                            Icons.email,
+                            color: Color(0xFFFFA726),
+                          ),
+                          filled: true,
+                          fillColor: Color(0xFFF5F5F5),
+                          border: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(8)),
+                            borderSide: BorderSide.none,
+                          ),
+                          label: Text(
+                            'Email',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
                               color: Color(0xFFFFA726),
                             ),
-                            filled: true,
-                            fillColor: Color(0xFFF5F5F5),
-                            border: OutlineInputBorder(
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(8)),
-                              borderSide: BorderSide.none,
-                            ),
-                            label: Text(
-                              'Email',
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                color: Color(0xFFFFA726),
-                              ),
-                            ))),
-                    const SizedBox(height: 16),
-                    const TextField(
-                        obscureText: true,
-                        decoration: InputDecoration(
-                            prefixIcon: Icon(
-                              Icons.lock,
-                              color: Color(0xFFFFA726),
-                            ),
-                            filled: true,
-                            fillColor: Color(0xFFF5F5F5),
-                            border: OutlineInputBorder(
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(8)),
-                              borderSide: BorderSide.none,
-                            ),
-                            label: Text(
-                              'Password',
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                color: Color(0xFFFFA726),
-                              ),
-                            ))),
-                    const SizedBox(height: 10),
-                  //Align(
-                     // alignment: Alignment.centerRight,
-                      //child: TextButton(
-                        //onPressed: () {},
-                        //child: const Text(
-                          //'Forgot Password?',
-                          //style: TextStyle(
-                            //fontWeight: FontWeight.bold,
-                            //fontSize: 14,
-                            //color: Color(0xFF616161),
-                          //),
-                        //),
-                      //),
-                    //),
-                    const SizedBox(height: 30),
-                    ElevatedButton(
-                      onPressed: () {
-                        // Navigasi ke halaman homepage
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => const HomePage()),
-                        );
-                      },
-                      style: ElevatedButton.styleFrom(
-                        foregroundColor: Colors.white,
-                        backgroundColor: const Color(0xFFFFA726),
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 80, vertical: 12),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(25),
+                          ),
                         ),
                       ),
-                      child: const Text(
-                        'SIGN IN',
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 16),
+                      const SizedBox(height: 16),
+                      const TextField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          prefixIcon: Icon(
+                            Icons.lock,
+                            color: Color(0xFFFFA726),
+                          ),
+                          filled: true,
+                          fillColor: Color(0xFFF5F5F5),
+                          border: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(8)),
+                            borderSide: BorderSide.none,
+                          ),
+                          label: Text(
+                            'Password',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Color(0xFFFFA726),
+                            ),
+                          ),
+                        ),
                       ),
-                    ),
-                    const SizedBox(height: 20),
+                      const SizedBox(height: 10),
+                      Align(
+                        alignment: Alignment.centerRight,
+                        child: TextButton(
+                          onPressed: () {},
+                          child: const Text(
+                            'Forgot Password?',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 14,
+                              color: Color(0xFF616161),
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 30),
+                      ElevatedButton(
+                        onPressed: () {
+                          // Navigasi ke halaman homepage
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const HomePage(),
+                            ),
+                          );
+                        },
+                        style: ElevatedButton.styleFrom(
+                          foregroundColor: Colors.white,
+                          backgroundColor: const Color(0xFFFFA726),
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 80,
+                            vertical: 12,
+                          ),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(25),
+                          ),
+                        ),
+                        child: const Text(
+                          'SIGN IN',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 20),
                       Align(
                         alignment: Alignment.center,
                         child: Column(
@@ -130,34 +170,39 @@ class Signin extends StatelessWidget {
                             const Text(
                               "Don't have an account?",
                               style: TextStyle(
-                                  fontWeight: FontWeight.bold, color: Colors.grey),
+                                fontWeight: FontWeight.bold,
+                                color: Colors.grey,
+                              ),
                             ),
                             TextButton(
                               onPressed: () {
                                 Navigator.push(
                                   context,
                                   MaterialPageRoute(
-                                      builder: (context) => const Signup()),
+                                    builder: (context) => const Signup(),
+                                  ),
                                 );
                               },
                               child: const Text(
                                 "Sign up",
                                 style: TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 16,
-                                    color: Color(0xFFFFA726)),
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 16,
+                                  color: Color(0xFFFFA726),
+                                ),
+                              ),
                             ),
-                          ),
-                        ],
+                          ],
+                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
             ),
           ),
-        ),
-      ],
-    ));
+        ],
+      ),
+    );
   }
 }
